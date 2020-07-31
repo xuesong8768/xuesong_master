df87af1a026245c387b10d7cbe70c06dd8686201
xuesong <1205433692@qq.com>
2020-07-31 10:45:05 +0800

    init

---
+++ forest-demo/src/main/java/com/xuesong/forest/client/MyClient.java
@@ -0,0 +1,7 @@
+package com.xuesong.forest.client;
+
+/**
+ * Created By xuesong on 2020/7/31 10:38
+ */
+public interface MyClient {
+}
---
+++ forest-demo/src/test/java/com/xuesong/forest/ForestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuesong.forest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
